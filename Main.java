330465a2b625c5d617a6cb18a1e33d0b7262c062
Tarunjeet Singh <tarunjeetsingh.91@gmail.com>
2025-10-08 12:15:05 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        System.out.println("This is a java program");
+        System.out.println("Output from java program");
+        System.out.println(new Date());
+    }
+}
